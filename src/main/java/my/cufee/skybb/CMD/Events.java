39d5ea5dc35ef3a0a4ac56548837d919835fd0c9
Daniil <92633278+CufeeEz@users.noreply.github.com>
2023-09-20 17:16:23 +0300

    реализация метода спавна сундука

--- src/main/java/my/cufee/skybb/CMD/Events.java
+++ src/main/java/my/cufee/skybb/CMD/Events.java
@@ -1,18 +1,22 @@
 package my.cufee.skybb.CMD;
 
+import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 
 public class Events implements CommandExecutor {
     private final Random random;
@@ -33,27 +37,49 @@ public class Events implements CommandExecutor {
     private void setRandomChest(){
         Player targetPlayer = Bukkit.getPlayer(getRandomPlayer());
         Location LocationSpawnedChest = targetPlayer.getLocation();
-        LocationSpawnedChest.add(1.0, -1.0, 1.0);
-        LocationSpawnedChest.getBlock().setType(Material.CHEST);
+        LocationSpawnedChest.add(1.0 + getRandomAmountForSpawnChest(), -1.0, 1.0 + getRandomAmountForSpawnChest());
+
+        Block chestBlock = LocationSpawnedChest.getBlock();
+        chestBlock.setType(Material.CHEST);
+        Chest chest = (Chest) chestBlock.getState();
+
+        for (int i = 0; i < 27; i++) {
+            if (getRandomAmountForItemChest() >= 12 ){
+                ItemStack Item = new ItemStack(getRandomItem().getType(), getRandomAmountForItemChest());
+                chest.getInventory().setItem(i, Item);
+            }
+
+        }
         Bukkit.broadcastMessage("Заспавнился сундучок для "+ targetPlayer.getName() + " X= "+ ((int) LocationSpawnedChest.getX()) + " Y= "+ ((int) LocationSpawnedChest.getY()) + " Z= "+ ((int) LocationSpawnedChest.getZ()));
 
 
 
 
     }
+    private ItemStack getRandomItem() {
+        // Получение случайного типа предмета из перечисления Material
+        Material[] materials = Material.values();
+        Material randomMaterial = materials[random.nextInt(materials.length)];
 
+        return new ItemStack(randomMaterial);
+    }
     private String getRandomPlayer(){ // метод получения рандомного игрока из списка онлайна
         List<String> Players = new ArrayList<>();
-
         for (Player player : Bukkit.getOnlinePlayers()) {
             Players.add(player.getName()); // Берем имя игрока.
         }
         int maxPlayerOnServer = getRandomAmount(Bukkit.getOnlinePlayers().size());
-        // Bukkit.broadcastMessage("Hello" + Players.get(maxPlayerOnServer));
         return Players.get(maxPlayerOnServer);
     }
 
     private int getRandomAmount(int max) {
         return random.nextInt(max);
+    }
+    private int getRandomAmountForItemChest() {
+        // Получение случайного количества от 1 до 15
+        return random.nextInt(15) + 1;
+    }
+    private int getRandomAmountForSpawnChest(){
+        return random.nextInt(500 - -500 + 1) + -500;
     }
 }
